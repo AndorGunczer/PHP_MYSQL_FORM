ad385d52ccdd07cbe0fb11ee5fb9c3aa04fcb89c
AndorGunczer <andor.gunczer@gmail.com>
2022-04-19 20:58:47 +0200

    documentation to insert.php added

--- insert.php
+++ insert.php
@@ -1,9 +1,13 @@
 <?php
+// Initialization of variables required to establish database connection
+// Initializierung der Variablen zur Verbindung mit der Datenbank
 $url = "127.0.0.1";
 $user = "basic";
 $password = "basic";
 $db = "pet_test";
 
+// Initialization of the variables 
+// Initializierung der Variablen mit der Werten vom Formular.
 $name = $_POST['name'];
 $owner = $_POST['owner'];
 $species = $_POST['species'];
@@ -11,16 +15,17 @@ $sex = $_POST['sex'];
 $birth = $_POST['birth'];
 $death = $_POST['death'];
 
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+
+//Ein Versuch, eine Verbindung zur Datenbank herzustellen.
 $link = mysqli_connect($url, $user, $password, $db);
  
-// Check connection
+// Check die Verbindung
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
  
 // Attempt insert query execution
+// Versuch der Ausführung einer Insert-Abfrage
 $sql = "INSERT INTO pet (name, owner, species, sex, birth, death) VALUES ('{$name}', '{$owner}', '{$species}', '{$sex}', '{$birth}', '{$death}')";
 if(mysqli_query($link, $sql)){
     echo "Records inserted successfully.";
@@ -29,6 +34,5 @@ if(mysqli_query($link, $sql)){
 }
  
 // Close connection
+// Verbindung schließen
 mysqli_close($link);
-
-?>
